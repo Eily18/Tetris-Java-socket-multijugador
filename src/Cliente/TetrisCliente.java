8217fdca0557ab031fe93bdeda2e424e9d5cc81b
Eily18 <eilynmartinez1812@gmail.com>
2026-01-26 16:39:54 -0400

    limpieza de archivos .class

--- src/Cliente/TetrisCliente.java
+++ src/Cliente/TetrisCliente.java
@@ -1,5 +1,5 @@
 package Cliente; //ubicacion del archivo
-
+// este es el archivo que inicia todo
 public class TetrisCliente {
     public static void main(String[] args){
         new VentanaDeJuego();
